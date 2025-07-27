c936f1d5486fec33492006cc57e40ed9dcbe0d6d
Samuel Pardo <samuelpardo1997@gmail.com>
2025-07-27 01:38:02 -0500

    Improve the UX

--- app/routes.py
+++ app/routes.py
@@ -4,12 +4,20 @@ frontend_bp = Blueprint('frontend', __name__)
 
 @frontend_bp.route('/')
 def home():
+    """Página de inicio que explica la aplicación"""
     return render_template('index.html')
+
+@frontend_bp.route('/scraping.html')
+def scraping():
+    """Página con el formulario de scraping"""
+    return render_template('scraping.html')
 
 @frontend_bp.route('/comments.html')
 def comments():
+    """Página que muestra los comentarios analizados"""
     return render_template('comments.html')
 
 @frontend_bp.route('/analytics.html')
 def analytics():
+    """Página que muestra los análisis y gráficos"""
     return render_template('analytics.html')
